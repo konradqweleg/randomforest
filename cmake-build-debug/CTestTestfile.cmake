f27c0d905fd7a0e8952bdfc372fdefb851841e66
Konrad <konradnrog@gmail.com>
2023-04-18 22:41:37 +0200

    add tests library, added missed files

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Konrad/Documents/repo/randomforrest/randomforest
+# Build directory: C:/Users/Konrad/Documents/repo/randomforrest/randomforest/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[multiply_gtest]=] "tests")
+set_tests_properties([=[multiply_gtest]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Konrad/Documents/repo/randomforrest/randomforest/CMakeLists.txt;17;add_test;C:/Users/Konrad/Documents/repo/randomforrest/randomforest/CMakeLists.txt;0;")
---
+++ cmake-build-debug/RandomForest/Google_tests/lib/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Konrad/Documents/repo/randomforrest/randomforest/RandomForest/Google_tests/lib/googlemock
+# Build directory: C:/Users/Konrad/Documents/repo/randomforrest/randomforest/cmake-build-debug/RandomForest/Google_tests/lib/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
