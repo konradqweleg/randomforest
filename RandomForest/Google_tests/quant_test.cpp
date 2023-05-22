61f899523a0468ff0913480435faa89f6a355012
Konrad <konradnrog@gmail.com>
2023-05-22 21:26:43 +0200

    Add tests to quant double columns

--- RandomForest/Google_tests/quant_test.cpp
+++ RandomForest/Google_tests/quant_test.cpp
@@ -2,18 +2,33 @@
 #include "gtest/gtest.h"
 #include "cnumpy/cnumpy.h"
 #include "quant/quant.h"
+#include "csv/csv.h"
+#include "entropy/Entropy.h"
+#include "cnumpy/math/histogram_base.h"
 
-//CNUMPY_quant_column_double_betweeen_method_result_avg_group
-//To linkowanie jest złe
+
 
 TEST(quant_tests, quant_double_column) {
-    std::cout<<"x";
-    std::vector<double> double_column{30.0, 29.9, 20.0,10.0,31.0,40.0,50.0,16.0};
-    std::cout<<"x";
+    std::vector<double> double_column{30.0, 5.0,12.5,19.0,90.0};
     Cnumpy data = Cnumpy::of(double_column);
-    std::cout<<data;
-    Cnumpy result = quant_utility::quant_column_double_betweeen_method_result_avg_group(data,0,1600);
-//
-   std::cout<<result;
+    Cnumpy result = quant_utility::quant_column_double_betweeen_method_result_avg_group(data,0,2);
+    ASSERT_DOUBLE_EQ(42.5,result.get_xy_double(0,0));
+    ASSERT_DOUBLE_EQ(42.5,result.get_xy_double(0,1));
+    ASSERT_DOUBLE_EQ(42.5,result.get_xy_double(0,2));
+    ASSERT_DOUBLE_EQ(42.5,result.get_xy_double(0,3));
+    ASSERT_DOUBLE_EQ(85,result.get_xy_double(0,4));
+
+}
+
+
+TEST(quant_tests,quant_double_column_if_calculate_for_iris_dataset){
+    histogram* hist_strategy = new histogram_base();
+    Cnumpy::set_histogram_calculation_strategy(hist_strategy);
+
+    csv csv_reader;
+    Cnumpy data_for_test_entropy = csv_reader.read_cnumpy_from_csv("C:\\Users\\Konrad\\Documents\\repo\\randomforrest\\randomforest\\RandomForest\\datasets\\iris\\data.csv", ",");
+
+    Cnumpy results = quant_utility::quant_column_double_betweeen_method_result_avg_group(data_for_test_entropy,0,10);
+
 
 }
--- RandomForest/quant/quant.h
+++ RandomForest/quant/quant.h
@@ -14,34 +14,9 @@
 
 
 class quant_utility {
-//More flexibility is provided by the implementation for individual columns. Code to delete
-    /*
-    static Cnumpy quant_data_in_cnumpy(Cnumpy data, double percentage_group_in_split, int predict_column_index) {
-
-        double hundred_percentage = 1.0;
-        int quantity_group = hundred_percentage / percentage_group_in_split;
-
-        for (int column_index = 0; column_index < data.get_x_dimension(); column_index++) {
-
-            if (column_index == predict_column_index) {
-                continue;
-            }
-
-            if (data.get_type_columns()[column_index] == Type::double_type) {
-                std::vector<double> column = data.get_column_double<double>(column_index);
-                column = quant_column_double_to_int(column, quantity_group);
-
-            }
-
-        }
-
-        return data;
-    }
-*/
 
 public:
 
-    //this is only my method so is no quite optimal for example, parametrized is good option
     static Cnumpy quant_column_double_betweeen_method_result_avg_group(Cnumpy data,int column_index, int how_many_groups) {
 
         std::vector<double> result;
@@ -51,24 +26,19 @@ public:
 
         double diff_min_max = max_value - min_value;
         double step = diff_min_max / how_many_groups;
-        double threshold = min_value + step;
 
-        std::cout<<diff_min_max<<std::endl;
-        std::cout<<step<<std::endl;
-        std::cout<<threshold<<std::endl;
+
 
         double min_ =0.0;
         double max_ =1.0;
 
         for (double value: data.get_column_double(column_index)) {
             double depart = (value-min_value) / ((double)step);
-                std::cout<<"Przedzial : "<<depart<<std::endl;
 
                 for(int i =0;i<how_many_groups;++i){
                     if( depart >= min_ && depart < max_ ){
 
                         double department_range_avg =  (step * (i+1));
-                        std::cout<<"Prognoza "<<i<< " Wartosc:"<<department_range_avg<<std::endl;
                         result.push_back(department_range_avg);
 
                         break;
@@ -78,7 +48,6 @@ public:
 
                         double department_range_avg =  (step * (i+1));
                         result.push_back(department_range_avg);
-                        std::cout<<"Prognoza "<<i<< " Wartosc:"<<department_range_avg<<std::endl;
                     }
                     min_ +=1.0;
                     max_+=1.0;
@@ -89,64 +58,11 @@ public:
 
         }
 
-//        for (int i = 0; i < how_many_groups; i++) {
-//
-//            for (double value: data.get_column_double(column_index)) {
-//                if (value < threshold && value > threshold - step) {
-//
-//                    double department_range_avg = threshold + (threshold - (threshold-step))/2;
-//                    result.push_back(  department_range_avg);
-//                }
-//            }
-//            threshold += step;
-//
-//        }
 
         return Cnumpy::of(result);
     }
 
 
-    static Cnumpy CNUMPY_quant_column_double_betweeen_method_result_avg_group(Cnumpy data,int index_column, int how_groups) {
-        std::cout<<":D";
-        Cnumpy how_many_groups = Cnumpy::of(how_groups);
-
-        Cnumpy result = data[index_column];
-
-        Cnumpy min_value = data.get_min_value_in_column(index_column);
-        Cnumpy max_value = data.get_max_value_in_column(index_column);
-
-        Cnumpy diff_min_max = max_value - min_value;
-        Cnumpy step = diff_min_max / how_many_groups;
-        Cnumpy threshold = min_value + step;
-
-        std::cout<<diff_min_max;
-        std::cout<<step;
-        std::cout<<threshold;
-        for (Cnumpy i = 0; i < how_many_groups; ) {
-
-            for (int j=0;j<data.get_y_dimension();++j) {
-                Cnumpy value = data[index_column][j];
-                Cnumpy lower_band = threshold - step;
-                if ( (value < threshold) && (value > lower_band)) {
-
-                    Cnumpy lower = threshold-step;
-                    Cnumpy right_band = (threshold - lower);
-                    Cnumpy two = Cnumpy::of(2.0);
-                    Cnumpy avg_threshold = right_band/two;
-                    Cnumpy department_range_avg = threshold + right_band;
-                    result.set(j,department_range_avg);
-                    std::cout<<" Th: "<<threshold<<"  "<<department_range_avg<<std::endl;
-                }
-            }
-            threshold += step;
-
-
-            Cnumpy one = Cnumpy::of(1);
-            i +=one;
-        }
-
-        return result;
-    }
 
 
 
